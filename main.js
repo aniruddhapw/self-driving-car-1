d41487aeae395cb1eed7f33b2b6481cc339c0d9e
Aniruddha Prabhakar Wankhade <aniruddha@Aniruddhas-MacBook-Pro.local>
2025-02-28 16:50:49 +0530

    roads are done

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const canvas = document.getElementById('myCanvas');
 canvas.width=200;
 
 const ctx=canvas.getContext('2d');
-const car = new Car(100,100,30,50);
-
+const road= new Road(canvas.width/2,canvas.width);
+const car = new Car(road.getLaneCenter(9),100,30,50);
 animate();
 
 function animate(){
     car.update();
     canvas.height=window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }
