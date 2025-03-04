371e12a99b09d496cc7fc441a6600e74737c3da0
Aniruddha Prabhakar Wankhade <aniruddha@Aniruddhas-MacBook-Pro.local>
2025-03-04 13:49:39 +0530

    traffic added

--- sensor.js
+++ sensor.js
@@ -10,20 +10,29 @@ class Sensor{
 
     }
 
-    update(roadBorders){
+    update(roadBorders,traffic){
         this.#castRays();
         this.readings=[];
         for(let i=0;i<this.rays.length;i++){
-            this.readings.push(this.#getReading(this.rays[i],roadBorders));
+            this.readings.push(this.#getReading(this.rays[i],roadBorders,traffic));
         }
   }
 
-  #getReading(ray,roadBorders){
+  #getReading(ray,roadBorders,traffic){
     let touches=[];
     for(let i=0;i<roadBorders.length;i++){
         const touch=getIntersection(ray[0],ray[1],roadBorders[i][0],roadBorders[i][1]);
         if(touch){
             touches.push(touch);
+        }
+    }
+    for(let i=0;i<traffic.length;i++){
+       const poly=traffic[i].polygon;
+       for(let j=0;j<poly.length;j++){
+           const touch=getIntersection(ray[0],ray[1],poly[j],poly[(j+1)%poly.length]);
+           if(touch){
+               touches.push(touch);
+           }
         }
     }
     if(touches.length==0){
